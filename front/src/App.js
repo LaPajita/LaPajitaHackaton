903751832f42ca904f25b0c49ab4a324e9c8670d
MariaRomanDelonia <maria.roman@delonia.es>
2020-05-13 00:56:57 +0200

    conflicts resolved

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './componentes/Home';
+<<<<<<< HEAD
+=======
+import LogIn from './componentes/login/LogIn';
+>>>>>>> 6373bad4389a89522c229232ac3a40ecabd9b2da
 import MyContext from "./context";
 import "./App.css";
+import Menu from './componentes/menu/Menu';
 
 function App() {
   const [hooksState, setHooksState] = useState({});
@@ -12,8 +17,10 @@ function App() {
   return (
     <MyContext.Provider value={stateAndFunction}>
       <div className="App">
+        <Menu />
         <Router>
           <Route path="/" exact component={Home} />
+          <Route path="/LogIn" exact component={LogIn} />
         </Router>
       </div>
     </MyContext.Provider>
--- front/src/componentes/menu/Menu.jsx
+++ front/src/componentes/menu/Menu.jsx
@@ -1,4 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import '.Menu.scss';
-import { Modal } from 'reactstrap';
+import './Menu.scss';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+
+
+
+const Menu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+
+  return (
+    <div className='Menu'>
+      <Navbar id='myNavBar' color="danger" navbar-dark dark sticky-top expand="md">
+        <NavbarBrand href="/">
+          <img className='logo'
+            src='https://s6.gifyu.com/images/logo-blanco-lapajita.png'
+            alt='logo La Pajita'
+            width='100px'
+            height='auto' />
+        </NavbarBrand>
+        <NavbarToggler id='myToggler' onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar className='white'>
+          <Nav className="ml-auto super" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Mi Perfil</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Mis Reservas</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Mis Favoritos</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Menu;
