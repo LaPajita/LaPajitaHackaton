b44fac3b36bb99c9f63388aa7c815746d5146b61
Javiuty <javiiescuadra@gmail.com>
2020-05-12 17:36:04 +0200

    Funcionando JSON

--- back/app.js
+++ back/app.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000
+require('dotenv').config();
+const database = require('./conf');
+
+
+
+app.use(express.urlencoded({extended: false}));
+
+//RUTA JSON DE PAJITA
+app.get('/', (req, res)=>{
+  database.query('SELECT * FROM cocktails', (error, results)=>{
+    if(error){
+      console.log(error)
+      res.status(404).send(error)
+    }else{
+      console.log(results)
+      res.status(200).send(results)
+    }
+  })
+})
+
+//ABRIMOS PUERTO
+app.listen(PORT, ()=>{
+  console.log(`Conectado servidor en puerto ${PORT}`)
+})
 
