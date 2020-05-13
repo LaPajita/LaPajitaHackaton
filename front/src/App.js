ab66a0f2bfdc7fba5959eab266af91e3e8ef8314
solar <jarabelmonte@gmail.com>
2020-05-13 13:21:05 +0200

    registration form

--- front/src/App.js
+++ front/src/App.js
@@ -4,16 +4,13 @@ import Home from './componentes/Home';
 import MyContext from "./context";
 import "./App.css";
 import Menu from './componentes/menu/Menu';
-<<<<<<< HEAD
 import Registration from './componentes/Registration/Registration';
-=======
-import CarouselComponent from './componentes/Carousel/CarouselComponent';
-<<<<<<< HEAD
-=======
+// import CarouselComponent from './componentes/Carousel/CarouselComponent';
 import LogIn from './componentes/login/LogIn';
->>>>>>> 4c7a44323acd0fb375e8453112e3b7cedbaa1b93
->>>>>>> 87f6f4c75bbff80fc9829c39ef31008d5b32456a
 // import Side from './componentes/menu/Side';
+
+
+
 
 function App() {
   const [hooksState, setHooksState] = useState({});
@@ -23,28 +20,16 @@ function App() {
   return (
     <MyContext.Provider value={stateAndFunction}>
       <div className="App">
-<<<<<<< HEAD
-        {/* <Menu /> */}
-        <CarouselComponent />
+       {/* <CarouselComponent />  */}
         {/* <Side /> */}
-        
-        {/* <Router>
-          <Route path="/" exact component={Home} />
-          <Route path="/LogIn" exact component={LogIn} />
-        </Router> */}
-=======
         <Menu />
-        {/* <Side /> */}
         <Router>
           <Route path="/" exact component={Home} />
           <Route path="/LogIn" exact component={LogIn} />
-<<<<<<< HEAD
           <Route path="/Registration" exact component={Registration} />
-=======
-          <Route path='/barDetail' component={CarouselComponent}/>
->>>>>>> 87f6f4c75bbff80fc9829c39ef31008d5b32456a
+          {/* <Route path='/barDetail' component={CarouselComponent}/> */}
+
         </Router>
->>>>>>> 4c7a44323acd0fb375e8453112e3b7cedbaa1b93
       </div>
     </MyContext.Provider>
   );
