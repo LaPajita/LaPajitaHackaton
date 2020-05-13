35777cf7a834c4b59481c9e0018c501153b6c5d2
Natalia T <tutornatalia22@gmail.com>
2020-05-13 14:56:16 +0200

    subiendo cambios para poder hacer pull

--- front/src/App.js
+++ front/src/App.js
@@ -5,10 +5,6 @@ import MyContext from "./context";
 import "./App.css";
 import Menu from './componentes/menu/Menu';
 import CarouselComponent from './componentes/Carousel/CarouselComponent';
-<<<<<<< HEAD
-=======
-import LogIn from './componentes/login/LogIn';
->>>>>>> 4c7a44323acd0fb375e8453112e3b7cedbaa1b93
 // import Side from './componentes/menu/Side';
 
 function App() {
@@ -19,24 +15,14 @@ function App() {
   return (
     <MyContext.Provider value={stateAndFunction}>
       <div className="App">
-<<<<<<< HEAD
-        {/* <Menu /> */}
-        <CarouselComponent />
-        {/* <Side /> */}
-        
-        {/* <Router>
-          <Route path="/" exact component={Home} />
-          <Route path="/LogIn" exact component={LogIn} />
-        </Router> */}
-=======
         <Menu />
+     
         {/* <Side /> */}
         <Router>
           <Route path="/" exact component={Home} />
-          <Route path="/LogIn" exact component={LogIn} />
+          {/* <Route path="/LogIn" exact component={LogIn} /> */}
           <Route path='/barDetail' component={CarouselComponent}/>
         </Router>
->>>>>>> 4c7a44323acd0fb375e8453112e3b7cedbaa1b93
       </div>
     </MyContext.Provider>
   );
